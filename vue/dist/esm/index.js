dfa9fe39595a5cd69a49ea95e2486999da280c17
jejaz <jelenazigic777@gmail.com>
2022-03-07 12:54:49 +0100

    dist? for vue repo

---
+++ vue/dist/esm/index.js
@@ -0,0 +1,7 @@
+import UniswapVue from './uniswap/uniswap.vue';
+export default {
+    install: function (Vue) {
+        Vue.component('uniswap-vue', UniswapVue);
+    },
+};
+//# sourceMappingURL=index.js.map
