411c1218a236cba23d80fbeccf3b503b36a0d593
jejaz <jelenazigic777@gmail.com>
2022-03-24 18:57:29 +0100

    ffdf

--- shared/dist/cjs/ethereum-provider.js
+++ shared/dist/cjs/ethereum-provider.js
@@ -90,7 +90,8 @@ var EthereumProvider = /** @class */ (function () {
         try {
             //(chainId === 80001 || chainId ===137) ? MATIC.info(chainId) : ETH.info(chainId);
             simple_uniswap_sdk_1.ETH.info(chainId);
-            console.log('is supported chain ' + supportedNetworkTokens.find(function (t) { return t.chainId === chainId; }) !== undefined);
+            console.log("is supported chain");
+            console.log(supportedNetworkTokens.find(function (t) { return t.chainId === chainId; }) !== undefined);
             return (supportedNetworkTokens.find(function (t) { return t.chainId === chainId; }) !== undefined);
         }
         catch (error) {
